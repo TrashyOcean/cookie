25d174f28700a63371cfa78aab25799228eda323
Anthony Sainez <tsainez@gmail.com>
2019-02-05 11:15:01 -0800

    converted to tab bar architecture

--- cookie/ViewController.swift
+++ cookie/ViewController.swift
@@ -24,9 +24,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        /*
         let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(swipeAction(swipe:)))
         leftSwipe.direction = UISwipeGestureRecognizer.Direction.left
         self.view.addGestureRecognizer(leftSwipe)
+        */
     }
 
     @IBAction func touchCookie(_ sender: UIButton) {
@@ -35,6 +37,7 @@ class ViewController: UIViewController {
     
 }
 
+/*
 extension UIViewController {
     @objc func swipeAction(swipe: UISwipeGestureRecognizer) {
         switch swipe.direction.rawValue {
@@ -47,51 +50,5 @@ extension UIViewController {
         }
     }
 }
+*/
 
-/*
- import UIKit
- 
- class ViewController: UIViewController {
- /*
- override func viewDidLoad() {
- super.viewDidLoad()
- // Do any additional setup after loading the view, typically from a nib.
- }
- */
- 
- var flipCount = 0 {
- didSet {
- flipCountLabel.text = "Flips: \(flipCount)"
- }
- }
- 
- @IBOutlet weak var flipCountLabel: UILabel!
- 
- @IBOutlet var cardButtons: [UIButton]!
- 
- var emojiChoices = ["🎃", "👻", "🎃", "👻"]
- 
- @IBAction func touchCard(_ sender: UIButton) {
- flipCount += 1
- if let cardNumber = cardButtons.index(of: sender) {
- flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
- } else {
- print("chosen card was not in cardButtons")
- }
- }
- 
- func flipCard(withEmoji emoji: String, on button: UIButton) {
- // print("flipCard(withEmoji: \(emoji))")
- 
- if button.currentTitle == emoji {
- button.setTitle("", for: UIControl.State.normal)
- button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
- } else {
- button.setTitle(emoji, for: UIControl.State.normal)
- button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
- }
- }
- 
- 
- }
- */
